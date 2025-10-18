f649a0f193dffd6868efc0892e4d36d423f17386
mustafa210-svt <sevot78@gmail.com>
2025-10-18 16:01:34 +0100

    Add files via upload

--- ping pong game.py
+++ ping pong game.py
@@ -1,4 +1,5 @@
 import tkinter 
+import time
 
 #drawing screen and geomtry
 screen = tkinter.Tk()
@@ -20,26 +21,50 @@ class Ball:
 
     def drop():
         pass
+    
+        
 
 
 b_object = Ball()
 
 class  Paddle:
-    def __init__(self,x1,y1,x2,y2):
-        self.paddles = c.create_rectangle(x1,y1,x2,y2,fill= "Red")
+    def __init__(self,x1,y1,x2,y2,c):
+        self.canvas = c
+        self.paddles = self.canvas.create_rectangle(x1,y1,x2,y2,fill= "Red")
         self.change_y = 0
 
 
-p1_object = Paddle(x1= 20, y1 = 200,x2= 30, y2= 310)
-p2_object = Paddle(x1 = 750, y1= 200, x2= 740, y2= 310)
-        
+
+    def move_padddle(self):
+        self.canvas.move(self.paddles,0,self.change_y)
+        self.position = self.canvas.coords(self.paddles)
 
 
+    def paddle_up(self,event):
+        self.change_y  = -1 
 
+    def paddle_down(self,event):
+        self.change_y = 1
 
+    
 
+p1_object = Paddle(x1= 20, y1 = 200,x2= 30, y2= 310, c = c)
+p2_object = Paddle(x1 = 750, y1= 200, x2= 740, y2= 310, c =c )
 
+c.bind_all("W", p1_object.paddle_up)
+while 1:
+    p1_object.move_padddle()
+    p2_object.move_padddle()
+
+    screen.update_idletasks()
+    screen.update()
+    time.sleep(0.01)
 
 
 screen.mainloop()
+
+
+
+
+
 
