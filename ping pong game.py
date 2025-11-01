6be595e93aaa5d118930c1a2a1234be7b461eaed
mustafa210-svt <sevot78@gmail.com>
2025-11-01 15:59:42 +0000

    Add files via upload

--- ping pong game.py
+++ ping pong game.py
@@ -11,31 +11,47 @@ c = tkinter.Canvas(screen, bg = "black")
 c.pack(fill="both", expand=True)
 c.create_line(400,0,400,500,width= 1,fill = "white")
 c.create_oval(350,200,450,300,width= 1,outline= "white")
+score = c.create_text(400,30, font = ("arial", 35, "bold"), text= "0    :    0",fill= "White")
+
+
 
 class Ball():
     def __init__(self,c):
        self.canvas = c 
-       self.ball = c.create_oval(380,230,400,250, width= 1, fill= "yellow")
-       self.change_x = 5
-       self.change_y =  -5
+       self.ball = c.create_oval(390,240,410,260, width= 1, fill= "yellow")
+       self.change_x = 3
+       self.change_y =  -3
+       self.p1_score = 0
+       self.p2_score = 0
+       
+
+
 
     def hit_paddle(self,paddle1,paddle2):
+       
+
         p1 = self.canvas.coords(paddle1.paddles)
         p2 = self.canvas.coords(paddle2.paddles)
-        ba = self.canvas.coords(b_object)
+        ba = self.canvas.coords(self.ball)
 
+        #left paddle (Y coordinate)
         if p1[1] < ba[3] and p1[3] > ba[1]:
+            # left paddle (X coordinate) 
             if p1[2] > ba[0] and p1[0] < ba[0]:
                 self.change_x = 5
         
         
-
-        if p2[1] < ba[3] and p1[3] > ba[1]:
+        #right paddle
+        if p2[1] < ba[3] and p2[3] > ba[1]:
             if p2[2] > ba[2] and p2[0] < ba[2]:
                 self.change_x = -5
 
-        
-                   
+       
+
+    
+            
+                    
+
 
     def move_ball(self):
         self.canvas.move(self.ball, self.change_x, self.change_y)
@@ -43,16 +59,30 @@ class Ball():
 
         if self.position[2] >= 800 :
             self.change_x = -5
+            self.p1_score = self.p1_score + 1
+            
+            
 
         if self.position[0] <= 0:
             self.change_x =  5
+            self.p2_score = self.p2_score + 1
 
         if self.position[1]  <= 0:
             self.change_y = 5 
+            
 
 
         if self.position[3] >= 500:
             self.change_y = -5
+            
+
+        c.itemconfigure(score,text= str(self.p1_score) + "   :   " + str(self.p2_score))
+
+        if self.p1_score == 3:
+            c.create_text(400,250,font= ("arial", 80, "bold"), fill= "white", text= "Game Over \n Player 1 wins ! ")
+
+        if self.p2_score == 3:
+            c.create_text(400,250, font= ("arial", 80, "bold"), fill= "white", text= "Game Over \n Player 2 wins ! ")
 
    
         
@@ -87,7 +117,7 @@ class  Paddle():
     
 #objects for paddle
 p1_object = Paddle(x1= 20, y1 = 200,x2= 30, y2= 310, c1 = c)
-p2_object = Paddle(x1 = 750, y1= 200, x2= 740, y2= 310, c1 =c )
+p2_object = Paddle(x1 = 770, y1= 200, x2= 780, y2= 310, c1 =c )  
 
 #moving left paddle
 c.bind_all("<w>", p1_object.paddle_up)
@@ -103,6 +133,7 @@ def w():
     p1_object.move_padddle()
     p2_object.move_padddle()
     b_object.move_ball()
+    b_object.hit_paddle(p1_object,p2_object)
     screen.update_idletasks()
     screen.update()
     screen.after(10,w)
@@ -113,9 +144,3 @@ w()
 
 
 screen.mainloop()
-
-
-
-
-
-
